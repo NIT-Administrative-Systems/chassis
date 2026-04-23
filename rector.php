e3340b51045709de05ab5b70417d4922a7ecffff
Danny Foster <danny@northwestern.edu>
2026-04-23 08:35:32 -0500

    chore: initial project scaffolding
    Package metadata, licensing, and tooling configuration for the chassis
    Composer package: composer.json, LICENSE, .editorconfig, .gitattributes
    (with export-ignore entries so dev files don't ship in archives),
    .gitignore, Pint (pint.json), PHPStan (phpstan.neon), PHPUnit
    (phpunit.xml with coverage source include/exclude lists), and Rector
    (rector.php) configured for PHP 8.3 with the PHP_83 rule set.

---
+++ rector.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
+use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\PHPUnit;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
+use Rector\TypeDeclaration\Rector\Closure\AddClosureVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\Closure\ClosureReturnTypeRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        SetList::PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+    )
+    ->withSkip([
+        __DIR__ . '/vendor',
+        AddClosureVoidReturnTypeWhereNoReturnRector::class,
+        ReturnBinaryOrToEarlyReturnRector::class,
+        ClosureReturnTypeRector::class,
+        AddArrowFunctionReturnTypeRector::class,
+        RemoveParentCallWithoutParentRector::class,
+        CompleteDynamicPropertiesRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withRules([
+        // PHPUnit: annotations to attributes + provider conventions
+        PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector::class,
+        PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector::class,
+        PHPUnit\PHPUnit110\Rector\Class_\NamedArgumentForDataProviderRector::class,
+        PHPUnit\PHPUnit70\Rector\Class_\RemoveDataProviderTestPrefixRector::class,
+        PHPUnit\PHPUnit100\Rector\Class_\PublicDataProviderClassMethodRector::class,
+        PHPUnit\PHPUnit100\Rector\Class_\StaticDataProviderClassMethodRector::class,
+
+        // PHPUnit: assertion modernizations + code quality
+        PHPUnit\PHPUnit90\Rector\MethodCall\ReplaceAtMethodWithDesiredMatcherRector::class,
+        PHPUnit\PHPUnit80\Rector\MethodCall\SpecificAssertContainsRector::class,
+        PHPUnit\PHPUnit100\Rector\MethodCall\PropertyExistsWithoutAssertRector::class,
+        PHPUnit\CodeQuality\Rector\MethodCall\AssertNotOperatorRector::class,
+        PHPUnit\CodeQuality\Rector\MethodCall\AssertCompareOnCountableWithMethodToAssertCountRector::class,
+        PHPUnit\CodeQuality\Rector\MethodCall\FlipAssertRector::class,
+        PHPUnit\CodeQuality\Rector\FuncCall\AssertFuncCallToPHPUnitAssertRector::class,
+        PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector::class,
+    ])
+    ->withCache(
+        cacheDirectory: sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'rector',
+        cacheClass: FileCacheStorage::class,
+    )
+    ->withParallel();
